771396645791e6a8983fde0e81364fa80d08b663
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-05-07 17:19:09 +0200

    Fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {YouTubePlayerModule} from '@angular/youtube-player'
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { fakeBackendProvider } from './_helpers';
 
