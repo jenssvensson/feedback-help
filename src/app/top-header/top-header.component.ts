b1f35e511d652bf8b92abf1355001a602bb778fa
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-05-07 20:04:10 +0200

    Add responsive view for header

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,21 +1,27 @@
 import { AuthenticationService } from './../login/authentication.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-header',
   templateUrl: './top-header.component.html',
-  styleUrls: ['./top-header.component.scss']
+  styleUrls: ['./top-header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TopHeaderComponent implements OnInit {
 
   currentUser: any;
+  navbarOpen = false;
 
   constructor(private router: Router, private authenticationService: AuthenticationService) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-   }
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
   logout() {
