ec2ce76cee848259e4c24efed0c060ecbbce19e3
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-06-11 18:43:48 +0200

    Fix authentication link

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit {
         private authenticationService: AuthenticationService
     ) {
         // redirect to home if already logged in
-        if (this.authenticationService.isAuthenticated) {
+        if (this.authenticationService.isAuthenticatedStatus) {
             this.router.navigate(['/']);
         }
     }
