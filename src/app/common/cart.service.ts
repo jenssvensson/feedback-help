3ea4eebba0654efeba2d114d94ae77329e53d333
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-06-30 18:39:52 +0200

    Add cart logic

--- src/app/common/cart.service.ts
+++ src/app/common/cart.service.ts
@@ -1,20 +1,63 @@
+import { BehaviorSubject } from 'rxjs';
+import { CartProduct } from './cart.model';
 import { Injectable } from '@angular/core';
 import { Product } from '../products/Product.model';
+import isEqual from 'lodash/isEqual';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  public products: Product[];
+  private products: BehaviorSubject<CartProduct[]>;
+  public totalAmount: BehaviorSubject<number>;
+  public itemCount: BehaviorSubject<number>;
+
+  constructor() {
+    this.products = new BehaviorSubject<CartProduct[]>(this.getCart());
+    this.totalAmount = new BehaviorSubject<number>(0);
+    this.itemCount = new BehaviorSubject<number>(0);
+    this.products.subscribe(cart => {
+      let amount = 0;
+      cart.forEach((cartProduct: CartProduct) => {
+        amount = cartProduct.Product.price * cartProduct.quantity;
+      });
+      this.totalAmount.next(amount);
+    });
+   }
 
   addToCart(product: Product) {
-    console.log('addToCart called with: ', product);
+    const prod = this.products.getValue();
+    let items = 0;
+    const found = prod && prod.find(cartproduct => {
+      items = items + cartproduct.quantity;
+      if (isEqual(product, cartproduct.Product)) {
+        cartproduct.quantity++;
+        items++;
+        return true;
+      }
+      return false;
+    });
+    if (!found) {
+      prod.push({Product: product, quantity: 1});
+      items++;
+    }
+    this.updateCart(prod, items);
   }
+
+  private updateCart(cartProd: CartProduct[], items: number) {
+    this.products.next(cartProd);
+    this.itemCount.next(items);
+    localStorage.setItem('cart', JSON.stringify(cartProd));
+  }
+
+  public getCart(): CartProduct[] {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    return cart ? cart : [];
+  }
+
 
   removeFromCart(product: Product) {
     console.log('removeFromCart called', product);
   }
-
-  constructor() { }
 }
--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,3 +1,4 @@
+import { CartService } from './../common/cart.service';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
@@ -16,9 +17,11 @@ export class TopHeaderComponent implements OnInit {
   public totalPrice: number;
   public totalQuantity: any;
 
-  constructor(private router: Router, private authenticationService: AuthenticationService) {
+  constructor(private router: Router, private authenticationService: AuthenticationService, private cart: CartService) {
     this.authenticationService.isAuthenticated.subscribe(x => this.loggedOn = x);
     this.authenticationService.currentUser.subscribe(x => this.user = x);
+    this.cart.itemCount.subscribe(itemCount => this.totalQuantity = itemCount);
+    this.cart.totalAmount.subscribe(amount => this.totalPrice = amount);
   }
 
   ngOnInit(): void {
