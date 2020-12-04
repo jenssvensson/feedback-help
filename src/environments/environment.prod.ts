5fe331a1225b87bb460c631bb697c973c8ae9462
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-12-04 23:44:35 +0100

    Fix lint error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //TODO Andreas byt ut nedan mot ditt prod clientID från paypal
+  // TODO Andreas byt ut nedan mot ditt prod clientID från paypal
   paypalClientId: 'ATMoNP201BNWcaJ5nCFbRq05pKZogjeTUAaLF-Jqv3vMILcv0Nv-lrgTYwjB071NYBMKhvXD7T73ptCx',
   apiUrl: 'https://feedbackhjalpen.se/'
 };
