aa8f5a8fa95fe5f2a3f179d4b383f457b7b15f85
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-12-04 23:24:11 +0100

    Add upload functionality

--- src/app/products/product-data.ts
+++ src/app/products/product-data.ts
@@ -6,12 +6,14 @@ export const PRODUCTS: Product[] = [
     id: 1,
     name: 'Full Monty',
     price: 42,
-    description: 'Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.'
+    description: 'Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+    image: 'dummy'
   },
   {
     id: 2,
     name: 'Small Monty',
     price: 6599,
-    description: 'Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.'
+    description: 'Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+    image: 'dummy'
   }
 ];
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,27 +1,71 @@
+import { AlertService } from './../alert/alert.service';
+import { Doc } from './Doc.model';
+import { UploadService } from './upload.service';
+import { Subscription } from 'rxjs';
 import { AuthenticationService } from './../authentication/authentication.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnInit, OnDestroy {
 
   public user: any;
   public loggedOn: boolean;
+  public interactionText: Doc;
+  subscription: Subscription;
 
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(
+      private authenticationService: AuthenticationService,
+      private uploadService: UploadService,
+      private alertService: AlertService)
+    {
     this.authenticationService.currentUser.subscribe(x => this.user = x);
     this.authenticationService.isAuthenticated.subscribe(x => this.loggedOn = x);
   }
 
   ngOnInit(): void {
+    this.getDocsData();
+  }
+
+  getDocsData() {
+    console.log('getDocsData');
+    this.subscription = this.uploadService.getDocs().subscribe(
+      doc => {
+        this.interactionText =  doc as Doc;
+      },
+      error => {
+        const options = {
+          autoClose: true,
+          keepAfterRouteChange: true
+        };
+        this.alertService.error(error.error.message, options);
+      });
   }
 
   uploadData() {
     console.log('Upload Data');
-    // TODO upload text from text area and notify the user
+    // tslint:disable-next-line:no-shadowed-variable
+    console.log(this.interactionText);
+    this.uploadService.saveDoc(this.interactionText).subscribe(response => {
+      console.log('Doc has been updated in database: ', response);
+      const options = {
+        autoClose: true,
+        keepAfterRouteChange: true
+      };
+      this.alertService.success('Text saved, well done!!!', options);
+    }, error => {
+      const options = {
+        autoClose: true,
+        keepAfterRouteChange: true
+      };
+      this.alertService.error(error.error.message, options);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
