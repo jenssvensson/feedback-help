016ff5ef64b8f71bbb28bd5ae82422a50c057c0e
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-12-07 20:55:21 +0100

    Fix prod apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // TODO Andreas byt ut nedan mot ditt prod clientID från paypal
   paypalClientId: 'AYmmNhJwS4xaziCV03seFH0R6czjhGWMBnmN1iK-ORlqBtwmAmHVAbavLF2y_9vbieOs5ERKQNb5XS6f',
-  apiUrl: 'https://3.127.57.74/feedbackhelp-real-2/'
+  apiUrl: 'http://3.127.57.74:8080/feedbackhelp-real-2/'
 };
