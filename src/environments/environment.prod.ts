feb1e5f74b29d5d0460191c74ec4f0c7eabf4fae
Jens Svensson <jens.fredrik.svensson@gmail.com>
2020-12-07 08:22:02 +0100

    Fix angular config. Set apiurl configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // TODO Andreas byt ut nedan mot ditt prod clientID från paypal
-  paypalClientId: 'ATMoNP201BNWcaJ5nCFbRq05pKZogjeTUAaLF-Jqv3vMILcv0Nv-lrgTYwjB071NYBMKhvXD7T73ptCx',
-  apiUrl: 'https://feedbackhjalpen.se/'
+  paypalClientId: 'AYmmNhJwS4xaziCV03seFH0R6czjhGWMBnmN1iK-ORlqBtwmAmHVAbavLF2y_9vbieOs5ERKQNb5XS6f',
+  apiUrl: 'https://3.127.57.74/feedbackhelp-real-2/'
 };
