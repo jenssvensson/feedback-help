8ad6013bd8baa23808eccd5c24cb05cb19a37908
Andreas Paulsson <paulssona@gmail.com>
2020-12-08 17:12:28 +0100

    Change prod APIurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // TODO Andreas byt ut nedan mot ditt prod clientID från paypal
   paypalClientId: 'AYmmNhJwS4xaziCV03seFH0R6czjhGWMBnmN1iK-ORlqBtwmAmHVAbavLF2y_9vbieOs5ERKQNb5XS6f',
-  apiUrl: 'http://3.127.57.74:8080/feedbackhelp-real-2/'
+  apiUrl: 'https://feedbackhjalpen.se:8443/feedbackhelp-real-2/'
 };
